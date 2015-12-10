21583646eedc730b42c655b830ac0bf52f472d9a
Mnoocvetko <mnoocvetko@abv.bg>
2015-12-10 16:51:46 +0200

    encript decript

--- excerciseThree/Encript.java
+++ excerciseThree/Encript.java
@@ -10,33 +10,35 @@ public class Encript {
 		System.out.println("Enter text: ");
 		String text = input.next();
 
+		char[][] key = {{ 'B', 'M', 'S', 'J', 'C', }, 
+	                    { 'I', 'F', 'N', 'T', 'K', },
+	                    { 'R', 'L', 'A', 'H', 'E', }, 
+	                    { 'D', 'O', 'G', 'Q', 'V', }, 
+	                    { 'U', 'W', 'X', 'Y', 'P', },};
 		
 
 		System.out.println(text = textToUpperCase(text));
 
+		System.out.println(text = encripting(text, key));
 		
-		System.out.println(text = encripting(text));
-		
-		
+		System.out.println(text = decripting(text, key));
 
 	}
 	
+	
+	
 	public static String textToUpperCase(String text) {
 		if (text.length() % 2 != 0) {
-			text += "c";
+			text += "p";
 		}
 		text = text.toUpperCase();
 		
 		return text;
 	}
 
-	public static String encripting(String text) {
+	public static String encripting(String text, char[][] key) {
 
-		char[][] encripting = {{ 'B', 'M', 'S', 'J', 'C', }, 
-				               { 'I', 'F', 'N', 'T', 'K', },
-				               { 'R', 'L', 'A', 'H', 'E', }, 
-				               { 'D', 'O', 'G', 'Q', 'V', }, 
-				               { 'U', 'W', 'X', 'Y', 'P', },};
+	
 
 		char firstChar = 0;
 		char secondChar = 0;
@@ -48,31 +50,35 @@ public class Encript {
 		int secondRow = 0;
 		int secondCol = 0;
 
+		
+		// get elements from text
 		for (int i = 0; i < text.length(); i += 2) {
 			firstChar = text.charAt(i);
 			secondChar = text.charAt(i + 1);
-			for (int row = 0; row < encripting.length; row++) {
-				for (int col = 0; col < encripting[row].length; col++) {
-					if (firstChar == encripting[row][col]) {
+			
+			//find coordinates of two elements 
+			for (int row = 0; row < key.length; row++) {
+				for (int col = 0; col < key[row].length; col++) {
+					if (firstChar == key[row][col]) {
 						firstRow = row;
 						firstCol = col;
 					}
-					if (secondChar == encripting[row][col]) {
+					if (secondChar == key[row][col]) {
 						secondRow = row;
 						secondCol = col;
 					}
 				}
 			}
-
+			//check for two elements on one col
 			if (firstCol == secondCol) {
 
 				firstRow++;
 				secondRow++;
 
-				if (firstRow >= encripting.length) {
+				if (firstRow >= key.length) {
 					firstRow = 0;
 				}
-				if (secondRow >= encripting.length) {
+				if (secondRow >= key.length) {
 					secondRow = 0;
 				}
 
@@ -81,23 +87,96 @@ public class Encript {
 			if (firstRow == secondRow) {
 				firstCol++;
 				secondCol++;
-				if (firstCol >= encripting.length) {
+				if (firstCol >= key.length) {
 					firstCol = 0;
 				}
-				if (secondCol >= encripting.length) {
+				if (secondCol >= key.length) {
 					secondCol = 0;
 				}
-				newText += encripting[firstRow][firstCol];
-				newText += encripting[secondRow][secondCol];
+				newText += key[firstRow][firstCol];
+				newText += key[secondRow][secondCol];
 
 			} else {
-				newText += encripting[firstRow][secondCol];
-				newText += encripting[secondRow][firstCol];
+				newText += key[firstRow][secondCol];
+				newText += key[secondRow][firstCol];
 			}
 
 		}
 
 
+		return newText;
+	}
+	
+	public static String decripting(String text, char[][] key) {
+		
+//		char[][] decripting = {{ 'B', 'M', 'S', 'J', 'C', }, 
+//	                           { 'I', 'F', 'N', 'T', 'K', },
+//	                           { 'R', 'L', 'A', 'H', 'E', }, 
+//	                           { 'D', 'O', 'G', 'Q', 'V', }, 
+//	                           { 'U', 'W', 'X', 'Y', 'P', },};
+		
+		char firstChar = 0;
+		char secondChar = 0;
+
+		String newText = "";
+
+		int firstRow = 0;
+		int firstCol = 0;
+		int secondRow = 0;
+		int secondCol = 0;
+		
+		for (int i = 0; i < text.length(); i += 2) {
+			firstChar = text.charAt(i);
+			secondChar = text.charAt(i + 1);
+			for (int row = 0; row < key.length; row++) {
+				for (int col = 0; col < key[row].length; col++) {
+					if (firstChar == key[row][col]) {
+						firstRow = row;
+						firstCol = col;
+					}
+					if (secondChar == key[row][col]) {
+						secondRow = row;
+						secondCol = col;
+					}
+				}
+			}
+			
+			if (firstCol == secondCol) {
+
+				firstRow--;
+				secondRow--;
+				
+				if(firstRow < 0) {
+					firstRow = key.length -1;
+				}
+				if(secondRow < 0) {
+					firstRow = key.length -1;
+				}
+
+			}
+			
+			if(firstRow == secondRow) {
+				firstCol--;
+				secondCol--;
+				
+				if(firstCol < 0) {
+					firstCol = key.length - 1;
+				}
+				if(secondCol < 0) {
+					secondCol = key.length - 1;
+				}
+				
+				newText += key[firstRow][firstCol];
+				newText += key[secondRow][secondCol];
+			}
+			else {
+				newText += key[firstRow][secondCol];
+				newText += key[secondRow][firstCol];
+			}
+			
+			
+			
+		}
 		return newText;
 	}
 	
